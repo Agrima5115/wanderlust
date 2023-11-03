b53c908ab2ff1a75350a68c2ee1973dc956ea461
sneha-1003 <uppara.sneha2021@vitstudent.ac.in>
2023-11-03 10:59:52 +0530

    Initial commit

---
+++ app/src/main/java/com/example/wanderlust/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.wanderlust
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.google.firebase.auth.FirebaseAuth
+
+@SuppressLint("SuspiciousIndentation")
+@Composable
+fun myapp(){
+    var firebaseAuth: FirebaseAuth = FirebaseAuth.getInstance()
+    val navController=rememberNavController()
+    NavHost(
+        navController=navController,
+        startDestination="Welcome"
+    ){
+        composable("Welcome"){Welcome(navController)}
+        composable("SignUp"){ SignUp(navController)}
+        composable("Login"){ Login(navController)}
+    }
+}
+
+class MainActivity:ComponentActivity(){
+    private lateinit var firebaseAuth: FirebaseAuth
+    override fun onCreate(savedInstanceState:Bundle?){
+        super.onCreate(savedInstanceState)
+        firebaseAuth = FirebaseAuth.getInstance()
+        setContent{
+            myapp()
+        }
+
+
+    }
+}
---
+++ app/src/main/java/com/example/wanderlust/Welcome.kt
@@ -0,0 +1,73 @@
+package com.example.wanderlust
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+
+
+@Composable
+fun Welcome(navController: NavController) {
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.img),
+
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
+
+        )
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+                .verticalScroll(rememberScrollState()), // Add vertical scrolling
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.End
+        ) {
+
+            Button(
+
+                modifier = Modifier
+                    .width(190.dp)
+                    .height(56.dp)
+                    .padding(start = 0.dp, end = 16.dp),
+
+                colors = ButtonDefaults.buttonColors( Color(0xFF4169e1),
+                    contentColor = Color.White),
+                onClick = { navController.navigate("Login") },
+            ) {
+                Text(
+                    "Get Started",
+                    fontSize = 20.sp
+                )
+            }
+        }
+    }
+}
