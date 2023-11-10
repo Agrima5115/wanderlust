a9e9712a8cbba2e3881af429dfab845a93dd9641
chandravamsy25 <90548992+chandravamsy25@users.noreply.github.com>
2023-11-10 14:05:40 +0530

    Update MainActivity.kt

--- app/src/main/java/com/example/wanderlust/MainActivity.kt
+++ app/src/main/java/com/example/wanderlust/MainActivity.kt
@@ -24,7 +24,8 @@ fun myapp(){
         composable("Welcome"){Welcome(navController)}
         composable("SignUp"){ SignUp(navController)}
         composable("Login"){ Login(navController)}
-        composable("Bookmark"){ Bookmark(navController) }
+        composable("Bookmark"){ Bookmark(navController)}
+        composable("Profile"){ Profile(navController)}
     }
 }
 
